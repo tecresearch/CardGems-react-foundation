b689b42d55848ffcfcd7d2fa6701ab1bdc601275
Mr.Brijesh <bnlv1212@gmail.com>
2024-07-28 20:42:09 +0530

    Tailwindcss installed and tested

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Card } from './components/Card';
+import { cardData } from './data/data'; // Import the data
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="p-4">
+      <h1 className="bg-gray-100 text-black p-4 text-center">Products</h1>
+      <div className="flex flex-wrap justify-center gap-4">
+        {cardData.map(card => (
+          <Card
+            key={card.id}
+            title={card.title}
+            description={card.description}
+            imageUrl={card.imageUrl}
+          />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export function Card({ title, description, imageUrl }) {
+  return (
+    <div className="w-[300px] rounded-md border">
+      <img
+        src={imageUrl}
+        alt={title}
+        className="h-[200px] w-full rounded-md object-cover"
+      />
+      <div className="p-4">
+        <h1 className="text-lg font-semibold">{title}</h1>
+        <p className="mt-3 text-sm text-gray-600">
+          {description}
+        </p>
+        <button
+          type="button"
+          className="mt-4 rounded-sm bg-black px-2.5 py-1 text-[10px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+        >
+          Read
+        </button>
+      </div>
+    </div>
+  );
+}
