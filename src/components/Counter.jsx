dbc3d8ce616bb279cbcb225f2d149a94852dc8fc
Mr.Brijesh <bnlv1212@gmail.com>
2024-07-30 10:36:59 +0530

    new feature added counter.jsx in component folder

---
+++ src/components/Counter.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function Counter() {
+    const [count, countSet]= useState(0);
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Counter
